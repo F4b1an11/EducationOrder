2176683e50b6402363ffe26219715d8be3d338de
Fabian <ochoa.fabian15@gmail.com>
2025-04-30 14:14:58 -0500

    delete backend

--- backend/sessions/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SessionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sessions'
-    label = 'learning_sessions'  # Add a unique label to avoid conflict with Django's sessions
